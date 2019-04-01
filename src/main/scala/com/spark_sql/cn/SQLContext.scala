32c81f852baa50a4d937657873a621f0cca586b1
LoneSurvivor1993 <tanzebin@foxmail.com>
2019-04-01 15:17:02 +0800

    Initial commit

---
+++ src/main/scala/com/spark_sql/cn/SQLContext.scala
@@ -0,0 +1,24 @@
+package com.spark_sql.cn
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SQLContext {
+  def main(args: Array[String]): Unit = {
+
+    val path = "file:///D:/people.json"
+
+    val sparkConf = new SparkConf().setAppName("SQLContextApp").setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new SQLContext(sc)
+
+
+    val people = sqlContext.read.format("json").load(path)
+    people.printSchema()
+    people.show()
+
+
+    sc.stop()
+
+  }
+}
